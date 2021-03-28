e3473a372b1673fefa15f64659def6c8b865d82e
kicbar <bartlomiejkicior@gmail.com>
2021-03-28 18:26:23 +0200

    add instalation instruction

--- config/create_db.sql
+++ config/create_db.sql
@@ -3,3 +3,15 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = true;  
 CREATE USER insurance_db IDENTIFIED BY passwd123;
 GRANT ALL PRIVILEGES TO insurance_db;
+
+/*
+order during installation:
+sequences.sql
+tables.sql
+synonyms.sql
+views.sql
+procedures.sql
+functions.sql
+triggers.sql
+insert_data.sql
+*/
