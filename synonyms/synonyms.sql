3a699b22bf0a3d66fd1cdddf620d72bd8293fae0
kicbar <bartlomiejkicior@gmail.com>
2021-04-07 23:17:54 +0200

    add synonyms

--- synonyms/synonyms.sql
+++ synonyms/synonyms.sql
@@ -1,1 +1,5 @@
 create synonym klienci FOR insurance_db.clients;
+create synonym kontakty FOR insurance_db.contacts;
+create synonym adresy FOR insurance_db.addresses;
+create synonym polisy FOR insurance_db.policies;
+create synonym roszczenia FOR insurance_db.claims;
