97cfa25b96902ba60d23fa9e6afd6753017337a1
kicbar <bartlomiejkicior@gmail.com>
2021-05-16 13:46:11 +0200

    set synonyms as public

--- synonyms/synonyms.sql
+++ synonyms/synonyms.sql
@@ -1,5 +1,5 @@
-create synonym klienci FOR insurance_db.clients;
-create synonym kontakty FOR insurance_db.contacts;
-create synonym adresy FOR insurance_db.addresses;
-create synonym polisy FOR insurance_db.policies;
-create synonym roszczenia FOR insurance_db.claims;
+create or replace public synonym klienci FOR insurance_db.clients;
+create or replace public synonym kontakty FOR insurance_db.contacts;
+create or replace public synonym adresy FOR insurance_db.addresses;
+create or replace public synonym polisy FOR insurance_db.policies;
+create or replace public synonym roszczenia FOR insurance_db.claims;
